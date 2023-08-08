fe479652884bfba1c10c638439376961d16da1aa
cipharius <cipharius@pm.me>
2023-08-08 18:51:07 +0300

    Updates nixosConfiguration generator logic

--- lib/generateNixosConfigurations.nix
+++ lib/generateNixosConfigurations.nix
@@ -3,57 +3,40 @@
 in
   l.mapAttrs (
     host: hostCfg: let
-      modulePaths =
-        l.filter (value: l.isString value)
-        (l.attrValues (
-          l.mapTree (path: path) "${inputs.self}/hosts/${host}"
-        ));
+      modulePaths = l.attrValues (inputs.haumea.lib.load {
+        src = "${inputs.self}/hosts/${host}";
+        loader = inputs.haumea.lib.loaders.path;
+      });
 
-      hostSystem =
-        if !(l.hasAttr "system" hostCfg)
-        then l.abort "Host \"${host}\" is missing a required field \"system\""
-        else if !(l.any (system: system == hostCfg.system) inputs.self.supportedSystems)
-        then l.abort "Host's \"${host}\" system \"${hostCfg.system}\" is not listed in supportedSystems"
-        else hostCfg.system;
+      customModulePaths = l.attrValues (inputs.haumea.lib.load {
+        src = "${inputs.self}/custom/modules";
+        loader = inputs.haumea.lib.loaders.path;
+      });
+
+      extraModules =
+        if l.hasAttr "modules" hostCfg
+        then hostCfg.modules
+        else [];
+
+      # hostSystem =
+      #   if !(l.hasAttr "system" hostCfg)
+      #   then l.abort "Host \"${host}\" is missing a required field \"system\""
+      #   else if !(l.any (system: system == hostCfg.system) inputs.self.supportedSystems)
+      #   then l.abort "Host's \"${host}\" system \"${hostCfg.system}\" is not listed in supportedSystems"
+      #   else hostCfg.system;
 
       hostNixpkgs =
         if l.hasAttr "defaultNixpkgs" hostCfg
         then hostCfg.defaultNixpkgs
         else inputs.nixpkgs;
-
-      hostPkgs = l.systemPkgs hostCfg.system hostNixpkgs;
-      lib = hostPkgs.lib;
+      # hostPkgs = l.systemPkgs hostCfg.system hostNixpkgs;
+      # lib = hostPkgs.lib;
     in
-      inputs.nixpkgs.lib.nixosSystem {
-        system = hostSystem;
-
+      hostNixpkgs.lib.nixosSystem {
         specialArgs = {
           inherit inputs host hostCfg;
           presets = inputs.self.presets;
         };
-        modules =
-          modulePaths
-          ++ [
-            {
-              networking.hostName = lib.mkDefault host;
-
-              # Enable nix-command and flakes implicitely, since this repository is designed for flakes only configuration
-              nix.settings.experimental-features = lib.mkDefault ["nix-command" "flakes"];
-
-              nix.registry.nixpkgs.flake = hostNixpkgs;
-
-              nixpkgs.pkgs = hostPkgs;
-              nix.nixPath = [
-                "nixpkgs=${hostNixpkgs}"
-              ];
-
-              environment.etc.nixos.source = inputs.self;
-
-              system.stateVersion =
-                if !(l.hasAttr "stateVersion" hostCfg)
-                then l.abort "Host \"${host}\" is missing a required field \"stateVersion\""
-                else hostCfg.stateVersion;
-            }
-          ];
+        modules = customModulePaths ++ extraModules ++ modulePaths;
       }
   )
