46d4cc2f5d835af912738ba7f769acd531b6f707
cipharius <cipharius@pm.me>
2023-08-08 18:43:05 +0300

    Adds custom NixOS module

---
+++ custom/modules/allow-unfree.nix
@@ -0,0 +1,20 @@
+{
+  config,
+  lib,
+  ...
+}: {
+  options = {
+    nixpkgs.allowUnfreeWhitelist = lib.mkOption {
+      default = [];
+      description = "A whitelist of allowed unfree packages";
+      type = with lib.types; listOf string;
+    };
+  };
+
+  config = {
+    nixpkgs.config.allowUnfreePredicate = (
+      pkg:
+        builtins.elem (lib.getName pkg) config.nixpkgs.allowUnfreeWhitelist
+    );
+  };
+}
