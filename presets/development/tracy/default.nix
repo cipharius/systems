5336a042e30eae60a8b536a98cc62d775689cf6b
cipharius <cipharius@pm.me>
2023-10-04 12:53:07 +0300

    Uses newer tracy checkout

--- presets/development/tracy/default.nix
+++ presets/development/tracy/default.nix
@@ -1,3 +1,16 @@
 {pkgs, ...}: {
-  environment.systemPackages = [pkgs.tracy];
+  environment.systemPackages = let
+  tracyVersion = "da1bc2b60eed1a217af18324e60bc5fbc334ff20";
+  in [
+    (pkgs.tracy.overrideAttrs (final: prev: {
+      version = tracyVersion;
+
+      src = pkgs.fetchFromGitHub {
+        owner = "wolfpld";
+        repo = "tracy";
+        rev = tracyVersion;
+        hash = "sha256-oS8bzWyygPlMoKsUfWzjLUQN645KY3VOfpYSI82Sx5Y=";
+      };
+    }))
+  ];
 }
