262174b2ba9a14908b6ed9d044f507165bc2d7ee
cipharius <cipharius@pm.me>
2023-11-10 19:56:28 +0200

    Adds agenix to system packages

--- presets/agenix.nix
+++ presets/agenix.nix
@@ -1,12 +1,20 @@
 {
   inputs,
   lib,
+  config,
   ...
-}: {
+}:
+let
+  system = config.nixpkgs.hostPlatform.system;
+in {
   imports = [
     inputs.agenix.nixosModules.default
   ];
   nix.registry.agenix.flake = inputs.agenix;
+
+  environment.systemPackages = [
+    inputs.agenix.packages.${system}.default
+  ];
 
   # Agenix requires all hosts to have a key pair
   services.openssh.enable = true;
