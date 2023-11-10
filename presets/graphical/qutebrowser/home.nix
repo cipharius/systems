5000337e6708011a120a29a825bb546fec798cdc
cipharius <cipharius@pm.me>
2023-11-10 19:01:12 +0200

    Fixes broken qutebrowser rendering

--- presets/graphical/qutebrowser/home.nix
+++ presets/graphical/qutebrowser/home.nix
@@ -1,5 +1,9 @@
 {pkgs, ...}: {
-  home.packages = [pkgs.qutebrowser];
+  home.packages = [
+    (pkgs.qutebrowser.override {
+      enableVulkan = false;
+    })
+  ];
 
   home.file = {
     ".config/qutebrowser/config.py".source = ./config.py;
