72961c61d430548739746e598ef901b1b2678879
cipharius <cipharius@pm.me>
2023-11-19 10:32:31 +0200

    Changes qutebrowser user agent to chromium
    Since qutebrowser now internally uses Qt with chromium anyway, might as well get better compatability.

--- presets/graphical/qutebrowser/config.py
+++ presets/graphical/qutebrowser/config.py
@@ -3,6 +3,7 @@ c.tabs.tabs_are_windows = True
 c.tabs.show = "multiple"
 c.downloads.location.directory = "~/downloads"
 c.content.pdfjs = True
+c.content.headers.user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.52 Safari/537.36"
 c.editor.command = ["kitty", "kak", "{}"]
 
 config.load_autoconfig()
