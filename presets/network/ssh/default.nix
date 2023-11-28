e2d99d689d2338b4b806714d9c9e83c90ec740e3
cipharius <cipharius@pm.me>
2023-11-28 12:10:57 +0200

    Updates permitted ssh MAC algorithms

--- presets/network/ssh/default.nix
+++ presets/network/ssh/default.nix
@@ -2,5 +2,14 @@
   services.openssh = {
     enable = true;
     openFirewall = true;
+
+    settings.Macs = [
+      "hmac-sha2-256"
+      "hmac-sha2-512"
+      "hmac-sha2-256-etm@openssh.com"
+      "hmac-sha2-512-etm@openssh.com"
+      "umac-128-etm@openssh.com"
+      "umac-128@openssh.com"
+    ];
   };
 }
