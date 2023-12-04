98d070929daf07bd7e7007f8c6eac051fabf4d2d
cipharius <cipharius@pm.me>
2023-12-04 13:39:05 +0100

    Fixes git user

--- presets/users/git/default.nix
+++ presets/users/git/default.nix
@@ -1,6 +1,12 @@
-{...}: {
+{pkgs, ...}: {
+  environment.systemPackages = [pkgs.git];
+
   users.users.git = {
+      isSystemUser = true;
+      group = "git";
       createHome = true;
       home = "/home/git";
+      useDefaultShell = true;
   };
+  users.groups.git = {};
 }
