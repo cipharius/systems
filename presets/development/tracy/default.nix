b715b7f29e33d20cee25f3596ea979997f8a251c
cipharius <cipharius@pm.me>
2024-02-13 12:11:40 +0200

    Builds tracy from source

--- presets/development/tracy/default.nix
+++ presets/development/tracy/default.nix
@@ -1,3 +1,16 @@
 {pkgs, ...}: {
-  environment.systemPackages = [ pkgs.tracy ];
+  environment.systemPackages = [
+    (pkgs.tracy.overrideAttrs (final: prev:
+    let version = "c4863d4324d0c8503f80f222d6a7b7d5f22aa597";
+    in {
+      version = "${version}";
+
+      src = pkgs.fetchFromGitHub {
+        owner = "wolfpld";
+        repo = "tracy";
+        rev = "${version}";
+        hash = "sha256-bBbgBABcbyoAi6unNAtFlCLCrVXy4XuQXK2cF3BjkTc=";
+      };
+    }))
+  ];
 }
