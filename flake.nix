43738d6e32c54574816ce01350594ed59d993b03
cipharius <cipharius@pm.me>
2024-10-07 10:06:14 +0300

    Updates flake inputs

--- flake.nix
+++ flake.nix
@@ -44,18 +44,18 @@
   # Remember to add flake inputs to nix.registry to ensure offline rebuilds
   # TODO Maybe possible to write nix check that detects unpinned input which would prevent offline rebuild?
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/release-23.11";
 
     nixpkgs-bleeding.url = "github:nixos/nixpkgs/master";
 
     # Core dependencies
     # Must be manually updated to prevent breakage
-    nixpkgs-core.url = "github:nixos/nixpkgs/45cef8da7cb34fb452d45ce31545dd4a5a36fcdf";
+    nixpkgs-core.url = "github:nixos/nixpkgs/2ac5652e83ddfca412a4b338714cb9afb27357d0";
 
     haumea.url = "github:nix-community/haumea/ec6350fd9353e7f27ce0e85d31f82e3ed73e4d70";
     haumea.inputs.nixpkgs.follows = "nixpkgs-core";
 
-    home-manager.url = "github:nix-community/home-manager/a9b36cbe9292a649222b89fdb9ae9907e9c74086";
+    home-manager.url = "github:nix-community/home-manager/2c78a57c544dd19b07442350727ced097e1aa6e6";
     home-manager.inputs.nixpkgs.follows = "nixpkgs-core";
 
     agenix.url = "github:ryantm/agenix/c2fc0762bbe8feb06a2e59a364fa81b3a57671c9";
