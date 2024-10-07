6b49d76e86922445f46833a721847116c5b271ad
cipharius <cipharius@pm.me>
2024-10-07 10:54:51 +0300

    Adds scanner config

--- presets/hardware/scanner/default.nix
+++ presets/hardware/scanner/default.nix
@@ -1,3 +1,9 @@
-{...}: {
+{pkgs, ...}: {
   hardware.sane.enable = true;
+  hardware.sane.brscan4.enable = true;
+  nixpkgs.allowUnfreeWhitelist = [
+    "brscan4"
+    "brother-udev-rule-type1"
+    "brscan4-etc-files"
+  ];
 }
