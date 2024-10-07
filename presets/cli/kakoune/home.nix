d49c2127094808af72ee5fa3ab7b1d18741cdb19
cipharius <cipharius@pm.me>
2024-10-07 10:52:19 +0300

    Removes LSP from kakoune

--- presets/cli/kakoune/home.nix
+++ presets/cli/kakoune/home.nix
@@ -5,7 +5,6 @@
         plugins = with kakounePlugins; [
           kak-ansi
           prelude-kak
-          kakoune-lsp
         ];
       };
     })
@@ -14,7 +13,6 @@
     universal-ctags # For ctags integration
     kakoune-cr # For session management and client spawning
     zls # Zig language server
-    clang-tools # For C/C++ language server
     calc # For mathematical expression evaluation
     xsel # For system clipboard integration
   ];
@@ -23,7 +21,6 @@
     ".config/kak/kakrc".source = ./kakrc;
     ".config/kak/scripts/lf.kak".source = ./lf.kak;
     ".config/kak/scripts/verifpal.kak".source = ./verifpal.kak;
-    ".config/kak-lsp/kak-lsp.toml".source = ./kak-lsp.toml;
   };
 
   home.sessionVariables.EDITOR = "kak";
