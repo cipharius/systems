275d8d2578912df500eb62338ce3405fc0b21836
cipharius <cipharius@pm.me>
2025-07-20 20:51:30 +0300

    Adds home network ipv6 prefix to personal-database auth table

--- presets/server/personal-database/default.nix
+++ presets/server/personal-database/default.nix
@@ -8,7 +8,10 @@ in
     ensureDatabases = ["personal"];
     enableTCPIP = true;
     settings.port = port;
-    authentication = "host personal all 192.168.8.0 255.255.255.0 password";
+    authentication = ''
+    host personal all 192.168.8.0 255.255.255.0 password
+    host personal all 2a03:ec00:b1a0:8bb::/64 password
+    '';
   };
   networking.firewall.allowedTCPPorts = [ port ];
 }
