a57c4adecb3e4ee4737a75b7db51b437cc65234d
cipharius <cipharius@pm.me>
2025-07-20 04:15:01 +0300

    Enables TCP access to personal-database

--- presets/server/personal-database/default.nix
+++ presets/server/personal-database/default.nix
@@ -6,7 +6,9 @@ in
   services.postgresql = {
     enable = true;
     ensureDatabases = ["personal"];
+    enableTCPIP = true;
     settings.port = port;
+    authentication = "host personal all 192.168.8.0 255.255.255.0 password";
   };
   networking.firewall.allowedTCPPorts = [ port ];
 }
